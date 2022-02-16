4d83dd5a638c16efe1147cc9414242589cb57f9d
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-02-16 18:30:17 +0300

    Two sections, different protocols

---
+++ ProtocolsRyabov/Cells/MyCell.swift
@@ -0,0 +1,41 @@
+//
+//  MyCell.swift
+//  ProtocolsRyabov
+//
+//  Created by Владимир Рябов on 16.02.2022.
+//
+
+import Foundation
+import UIKit
+
+class MyCell: CellParent, Rotate {
+        
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(label)
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: contentView.topAnchor),
+                label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+                label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            ])
+            textLabel = label
+            
+            contentView.backgroundColor = .lightGray
+            textLabel.textAlignment = .right
+            textLabel.font = .boldSystemFont(ofSize: 16)
+            textLabel.textColor = .systemBlue
+            textLabel.text = "1"
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    
+    
+}
+
---
+++ ProtocolsRyabov/Protocols.swift
@@ -0,0 +1,62 @@
+//
+//  Shakeable.swift
+//  ProtocolsRyabov
+//
+//  Created by Владимир Рябов on 16.02.2022.
+//
+
+
+import UIKit
+
+//MARK: - Protocol for the second type cell
+protocol Rotate {
+    
+}
+extension Rotate where Self: UIView {
+    func rotationCell(view : UIView, delay: TimeInterval) {
+        let animationDuration = 1.5
+        UIView.animate(withDuration: animationDuration, delay: delay, options: [UIView.AnimationOptions.autoreverse, UIView.AnimationOptions.repeat], animations: {
+            view.transform = CGAffineTransform(rotationAngle: (180.0 * .pi) / 180.0)
+        }, completion: nil)
+    }
+    
+    func randomColor(view : UIView, delay: TimeInterval) {
+        let animationDuration = 1.5
+        UIView.animate(withDuration: animationDuration, delay: delay, options: [UIView.AnimationOptions.autoreverse, UIView.AnimationOptions.repeat], animations: {
+            view.backgroundColor = .random()
+        }, completion: nil)
+    }
+}
+
+//MARK: - Protocol for the second type cell
+protocol ShakeAndFade {
+    
+}
+extension ShakeAndFade where Self: UIView {
+    func shake() {
+        let anim = CABasicAnimation(keyPath: "position")
+        anim.duration = 0.07
+        anim.repeatCount = .infinity
+        anim.autoreverses = true
+        anim.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 5.0, y: self.center.y))
+        anim.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 5.0, y: self.center.y))
+        layer.add(anim, forKey: "position")
+    }
+    
+    func fadeViewInThenOut(view : UIView, delay: TimeInterval) {
+
+        let animationDuration = 1.5
+        UIView.animate(withDuration: animationDuration, delay: delay, options: [UIView.AnimationOptions.autoreverse, UIView.AnimationOptions.repeat], animations: {
+            view.alpha = 0
+        }, completion: nil)
+    }
+    
+    func randomColor(view : UIView, delay: TimeInterval) {
+        let animationDuration = 1.5
+        UIView.animate(withDuration: animationDuration, delay: delay, options: [UIView.AnimationOptions.autoreverse, UIView.AnimationOptions.repeat], animations: {
+            view.backgroundColor = .random()
+        }, completion: nil)
+    }
+  
+}
+
